9420734b2c83844781b22066e3aee26102db133c
John D. Fisher <jdfenw@gmail.com>
2018-11-23 11:44:28 -0800

    feat(colorscheme): minimal working version

--- colorscheme/colorscheme.psm1
+++ colorscheme/colorscheme.psm1
@@ -35,9 +35,9 @@
  Wrapper for Microsoft console ColorTool
 
 #>
-Param()
+param()
 
-Function Set-ColorScheme
+function Set-ColorScheme
 {
 <#
 .Synopsis
@@ -57,94 +57,246 @@ Microsoft provides pre-built binaries or you can build from source. See RELATED 
 .Link
 https://github.com/Microsoft/console/tree/master/tools/ColorTool
 #>
-    [cmdletbinding(SupportsShouldProcess, ConfirmImpact='Low')]
-    Param(
-        [Parameter(Mandatory=$false)]
-        [switch] $Help,
+  [CmdletBinding(SupportsShouldProcess,ConfirmImpact = 'Medium', PositionalBinding=$false, DefaultParametersetName='current')]
+  param(
+    # Display help
+    [Parameter(Mandatory = $true,ParameterSetName='help')]
+    [switch]$Help,
 
-        [Parameter(Mandatory=$false)]
-        [switch] $Current,
+    # Display current color scheme
+    [Parameter(Mandatory = $true,ParameterSetName='current')]
+    [switch]$Current,
 
-        [Parameter(Mandatory=$false)]
-        [switch] $Quiet,
+    [Parameter(Mandatory = $false,ParameterSetName='apply')]
+    [Parameter(Mandatory = $false,ParameterSetName='xterm')]
+    [switch]$Quiet,
 
-        [Parameter(Mandatory=$false)]
-        [switch] $Both,
+    [Parameter(Mandatory = $false,ParameterSetName='apply')]
+    [switch]$Both,
 
-        [Parameter(Mandatory=$false)]
-        [switch] $Schemes,
+    [Parameter(Mandatory = $true,ParameterSetName='default')]
+    [switch]$Defaults,
 
-        [Parameter(Mandatory=$false)]
-        [switch] $Version,
+    [Parameter(Mandatory = $true,ParameterSetName='xterm')]
+    [switch]$Xterm,
 
-        [Parameter(Mandatory=$false)]
-        [Alias("Output")]
-        [string] $OutputFile,
+    # Display available color schemes
+    [Parameter(Mandatory = $true,ParameterSetName='schemes')]
+    [switch]$Schemes,
 
-        [Parameter(Mandatory=$false)]
-        [string] $Path = (Get-Item $PROFILE).Directory.FullName
-    )
+    # Display version
+    [Parameter(Mandatory = $true,ParameterSetName='version')]
+    [switch]$Version,
 
-     Begin {
-            if (-not $PSBoundParameters.ContainsKey('Verbose')) {
-                $VerbosePreference = $PSCmdlet.SessionState.PSVariable.GetValue('VerbosePreference')
-            }
-            if (-not $PSBoundParameters.ContainsKey('Confirm')) {
-                $ConfirmPreference = $PSCmdlet.SessionState.PSVariable.GetValue('ConfirmPreference')
-            }
-            if (-not $PSBoundParameters.ContainsKey('WhatIf')) {
-                $WhatIfPreference = $PSCmdlet.SessionState.PSVariable.GetValue('WhatIfPreference')
-            }
-            Write-Verbose ('[{0}] Confirm={1} ConfirmPreference={2} WhatIf={3} WhatIfPreference={4}' -f $MyInvocation.MyCommand, $Confirm, $ConfirmPreference, $WhatIf, $WhatIfPreference)
+    # Save current color scheme
+    [Parameter(Mandatory = $true,ParameterSetName='output')]
+    [string]$OutputFile,
 
-            # $ColorTool = (Get-Item $PROFILE).Directory
-            write-verbose "$Path"
-            $ColorToolExe = "ColorTool.exe"
-            try {
-                Write-Verbose "Looking for ColorTool in $Path"
-                $ColorTool= Get-Command "$Path\$ColorToolExe" -ErrorAction Stop
-            } catch [System.Management.Automation.CommandNotFoundException] {
-                Write-Verbose 'Looking for ColorTool in $env:PATH'
-                $ColorTool= Get-Command "$ColorToolExe" -ErrorAction Stop
-            } Finally {
-                write-verbose $ColorTool.Path
-            }
+    [Parameter(Position = 0,Mandatory = $true,ParameterSetName='apply')]
+    [Parameter(Position = 0,Mandatory = $true,ParameterSetName='default')]
+    [Parameter(Position = 0,Mandatory = $true,ParameterSetName='xterm')]
+    [string]$SchemeName,
 
-            if (Get-Command "$ColorToolExe" -ErrorAction Stop) {
-                $ColorToolExe = Get-Command "$ColorToolExe" | Resolve-Path
-                $ColorToolPath = (Get-Item $ColorToolExe).Directory
-                try {
-                    $ColorToolSchemes = Get-Item "$ColorToolPath\schemes" -ErrorAction Stop
-                } catch [System.Management.Automation.ItemNotFoundException] {
-                    Write-Verbose "$_.Exception.Message"
-                    Write-Verbose "ColorTool default location does not exist. Supply the full path to scheme files. See ColorTool --help"
-                }
-            }
-        }
+    [Parameter(Mandatory = $false)]
+    [string]$Path = (Get-Item $PROFILE).Directory.FullName
+  )
 
-    Process
-    {
-        & $ColorTool --schemes
-        # If ($Dark) {
-        #     $ColorToolScheme = "$PSScriptRoot\Solarized Dark Higher Contrast.itermcolors"
-        # }
-        # ElseIf ($Light) {
-        #     $ColorToolScheme = "$PSScriptRoot\solarized_light.itermcolors"
-        # }
-        # Else {
-        #     $ColorToolScheme = "$PSScriptRoot\solarized_dark.itermcolors"
-        # }
-        # Write-Verbose "You selected for the current session:`n         $ColorToolScheme"
-
-        # & $ColorToolExe  $ColorToolScheme
-
-        # If ($d) {
-        #     if ($PSCmdlet.ShouldProcess("Default Colorscheme")) {
-        #         Write-Verbose "Default updated to $ColorToolScheme`n Don't forget to save Defaults"
-        #         & $ColorToolExe  -d $ColorToolScheme
-        #     }
-        # }
+  begin {
+    if (-not $PSBoundParameters.ContainsKey('Verbose')) {
+      $VerbosePreference = $PSCmdlet.SessionState.PSVariable.GetValue('VerbosePreference')
     }
+    if (-not $PSBoundParameters.ContainsKey('Confirm')) {
+      $ConfirmPreference = $PSCmdlet.SessionState.PSVariable.GetValue('ConfirmPreference')
+    }
+    if (-not $PSBoundParameters.ContainsKey('WhatIf')) {
+      $WhatIfPreference = $PSCmdlet.SessionState.PSVariable.GetValue('WhatIfPreference')
+    }
+    Write-Verbose ('[{0}] Confirm={1} ConfirmPreference={2} WhatIf={3} WhatIfPreference={4}' -f $MyInvocation.MyCommand,$Confirm,$ConfirmPreference,$WhatIf,$WhatIfPreference)
+
+    $ColorToolExe = "ColorTool.exe"
+    try {
+      Write-Verbose "Looking for ColorTool in $Path"
+      $ColorTool = Get-Command "$Path\$ColorToolExe" -ErrorAction Stop
+    } catch [System.Management.Automation.CommandNotFoundException] {
+      Write-Verbose 'Looking for ColorTool in $env:PATH'
+      $ColorTool = Get-Command "$ColorToolExe" -ErrorAction Stop
+    } finally {
+      Write-Verbose ('Found {0}' -f $ColorTool.Path)
+    }
+
+    if ($Schemes) {
+      try {
+        Write-Verbose "Looking for schemes in $Path\schemes"
+          $ColorToolSchemes = Get-Item ('{0}\schemes' -f $Path) -ErrorAction Stop
+      } catch [System.Management.Automation.ItemNotFoundException] {
+        Write-Verbose "$_.Exception.Message"
+          Write-Verbose "ColorTool default location does not exist. Supply the full path to scheme files. See ColorTool --help"
+      }
+    }
+  }
+
+  process {
+    if ($Help) {
+      Get-Help Set-ColorScheme
+      $Parameters = '--help'
+    } elseif ($Version) {
+      $Parameters = '--version'
+    } elseif ($Schemes) {
+      $Parameters = '--schemes'
+    } elseif ($Current) {
+      $Parameters = '--current'
+    } elseif ($SchemeName) {
+      $Parameters = "$SchemeName"
+    }
+
+    if ($Quiet) {
+      $Parameters = "--quiet $Parameters"
+    }
+    if ($Both) {
+      $Parameters = "--both $Parameters"
+    }
+
+    if ($Xterm) {
+      $Parameters = "--xterm $Parameters"
+    }
+
+    if ($Defaults) {
+      $Parameters = "--defaults $Parameters"
+    }
+
+      $Item = $ColorTool | Format-List | Out-String | write-verbose
+      write-verbose "Parameters: $Parameters"
+  }
 }
 
-# Export-ModuleMember -Function Set-ColorScheme
+function Get-ColorScheme
+{
+<#
+.Synopsis
+Set the console color scheme to dark or light and save color scheme to defaults
+if desired.
+
+.Description
+The default is dark without updating the console color scheme defaults.
+
+If -d is selected, the color scheme is written to the console defaults. To
+save the defaults for future sessions, select Properties and OK and select
+Defaults and OK from the console menu.
+
+.Notes
+Microsoft provides pre-built binaries or you can build from source. See RELATED LINKS.
+
+.Link
+https://github.com/Microsoft/console/tree/master/tools/ColorTool
+#>
+  [CmdletBinding(SupportsShouldProcess,ConfirmImpact = 'Low', PositionalBinding=$false, DefaultParametersetName='current')]
+  param(
+    # Display help
+    [Parameter(Mandatory = $true,ParameterSetName='help')]
+    [switch]$Help,
+
+    # Display current color scheme
+    [Parameter(Mandatory = $true,ParameterSetName='current')]
+    [switch]$Current,
+
+    [Parameter(Mandatory = $false,ParameterSetName='apply')]
+    [Parameter(Mandatory = $false,ParameterSetName='xterm')]
+    [switch]$Quiet,
+
+    [Parameter(Mandatory = $false,ParameterSetName='apply')]
+    [switch]$Both,
+
+    [Parameter(Mandatory = $true,ParameterSetName='default')]
+    [switch]$Defaults,
+
+    [Parameter(Mandatory = $true,ParameterSetName='xterm')]
+    [switch]$Xterm,
+
+    # Display available color schemes
+    [Parameter(Mandatory = $true,ParameterSetName='schemes')]
+    [switch]$Schemes,
+
+    # Display version
+    [Parameter(Mandatory = $true,ParameterSetName='version')]
+    [switch]$Version,
+
+    # Save current color scheme
+    [Parameter(Mandatory = $true,ParameterSetName='output')]
+    [string]$OutputFile,
+
+    [Parameter(Position = 0,Mandatory = $true,ParameterSetName='apply')]
+    [Parameter(Position = 0,Mandatory = $true,ParameterSetName='default')]
+    [Parameter(Position = 0,Mandatory = $true,ParameterSetName='xterm')]
+    [string]$SchemeName,
+
+    [Parameter(Mandatory = $false)]
+    [string]$Path = (Get-Item $PROFILE).Directory.FullName
+  )
+
+  begin {
+    if (-not $PSBoundParameters.ContainsKey('Verbose')) {
+      $VerbosePreference = $PSCmdlet.SessionState.PSVariable.GetValue('VerbosePreference')
+    }
+    if (-not $PSBoundParameters.ContainsKey('Confirm')) {
+      $ConfirmPreference = $PSCmdlet.SessionState.PSVariable.GetValue('ConfirmPreference')
+    }
+    if (-not $PSBoundParameters.ContainsKey('WhatIf')) {
+      $WhatIfPreference = $PSCmdlet.SessionState.PSVariable.GetValue('WhatIfPreference')
+    }
+    Write-Verbose ('[{0}] Confirm={1} ConfirmPreference={2} WhatIf={3} WhatIfPreference={4}' -f $MyInvocation.MyCommand,$Confirm,$ConfirmPreference,$WhatIf,$WhatIfPreference)
+
+    $ColorToolExe = "ColorTool.exe"
+    try {
+      Write-Verbose "Looking for ColorTool in $Path"
+      $ColorTool = Get-Command "$Path\$ColorToolExe" -ErrorAction Stop
+    } catch [System.Management.Automation.CommandNotFoundException] {
+      Write-Verbose 'Looking for ColorTool in $env:PATH'
+      $ColorTool = Get-Command "$ColorToolExe" -ErrorAction Stop
+    } finally {
+      Write-Verbose ('Found {0}' -f $ColorTool.Path)
+    }
+
+    if ($Schemes) {
+      try {
+        Write-Verbose "Looking for schemes in $Path\schemes"
+          $ColorToolSchemes = Get-Item ('{0}\schemes' -f $Path) -ErrorAction Stop
+      } catch [System.Management.Automation.ItemNotFoundException] {
+        Write-Verbose "$_.Exception.Message"
+          Write-Verbose "ColorTool default location does not exist. Supply the full path to scheme files. See ColorTool --help"
+      }
+    }
+  }
+
+  process {
+    if ($Help) {
+      Get-Help Set-ColorScheme
+      $Parameters = '--help'
+    } elseif ($Version) {
+      $Parameters = '--version'
+    } elseif ($Schemes) {
+      $Parameters = '--schemes'
+    } elseif ($Current) {
+      $Parameters = '--current'
+    } elseif ($SchemeName) {
+      $Parameters = "$SchemeName"
+    }
+
+    if ($Quiet) {
+      $Parameters = "--quiet $Parameters"
+    }
+    if ($Both) {
+      $Parameters = "--both $Parameters"
+    }
+
+    if ($Xterm) {
+      $Parameters = "--xterm $Parameters"
+    }
+
+    if ($Defaults) {
+      $Parameters = "--defaults $Parameters"
+    }
+
+      $Item = $ColorTool | Format-List | Out-String | write-verbose
+      write-verbose "Parameters: $Parameters"
+  }
+}
